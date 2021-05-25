b45c51a43feebcf11fb765bbe03a80b2230db926
Hamza Nazli <chemhamza@gmail.com>
2021-05-25 18:03:48 +0530

    Checking for valid inputs

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 public class DecryptionFormController {
@@ -9,5 +11,17 @@ public class DecryptionFormController {
     public TextField txtText;
 
     public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+        String cipherText = txtCipherText.getText();
+        String key = txtKey.getText();
+
+        if(cipherText.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Please input a valid Cipher Text", ButtonType.OK).show();
+            return;
+        }
+        if(key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Please input a valid Key", ButtonType.OK).show();
+            return;
+        }
+
     }
 }
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 public class EncryptionFormController {
@@ -9,5 +11,23 @@ public class EncryptionFormController {
     public TextField txtCipherText;
 
     public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+        String text = txtText.getText();
+        String key = txtKey.getText();
+
+        if(text.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Please input a valid Text", ButtonType.OK).show();
+            return;
+        }
+        if(key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Please input a valid Key", ButtonType.OK).show();
+            return;
+        }
+
+        text += key;
+        System.out.println(text);
+
+
+
+
     }
 }
