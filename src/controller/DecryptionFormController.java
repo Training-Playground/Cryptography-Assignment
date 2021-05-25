3468abed1028e80dd4c438fdb9c6a8a543da3a03
Hamza Nazli <chemhamza@gmail.com>
2021-05-25 21:02:41 +0530

    added java doc comment describing the algorithm

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -23,6 +23,18 @@ public class DecryptionFormController {
             new Alert(Alert.AlertType.ERROR, "Please input a valid Key", ButtonType.OK).show();
             return;
         }
+
+        /**
+         * Returns the decrypted String text corresponding with the String cipherText and the String key given.
+         * Decrypting algorithm as follows--->
+         * Integer code of char at index of String ciphertext undergoes following changes in a for loop...
+         *          * if (index % 3 == 0), code equals to (code - 69)
+         *          * if (index % 3 == 1), code equals to (code - 194)
+         *          * else code equals to (code - 227)
+         * Corresponding to index, char values of codes assigned to textChar, String text is formulated accordingly
+         * String text becomes the subString from the index=0 to index = (length of text - length of key - length of "cinnamon")
+         * returns new String text
+         */
         txtText.setText(Crypt.decrypt(cipherText,key));
     }
 }
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -23,6 +23,18 @@ public class EncryptionFormController {
             new Alert(Alert.AlertType.ERROR,"Please input a valid Key", ButtonType.OK).show();
             return;
         }
+        /**
+         * Returns the encrypted String cipherText corresponding with the String text and the String key given.
+         * Encrypting algorithm as follows--->
+         * String key concatenated at the end with String "cinnamon".
+         * String text concatenated with the new String key.
+         * Integer code of char at index of new String text undergoes following changes in a for loop...
+         * if (index % 3 == 0), code equals to (code + 69)
+         * if (index % 3 == 1), code equals to (code + 194)
+         * else code equals to (code + 227)
+         * Corresponding to index, char values of codes assigned to cipherChar, cipherText is formulated accordingly
+         * returns new String cipherText
+         */
         txtCipherText.setText(Crypt.encrypt(text,key));
     }
 }
